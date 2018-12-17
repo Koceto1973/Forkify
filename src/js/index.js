ef5e5b9cd84f4d34ec13f79bdb1ba484095e2e7b
TeamK <koceto1973@yahoo.ca>
2018-12-17 22:53:41 +0200

    es6 strings example fixed

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,4 @@ console.log('Hi, from importing module!');
 import x from './test';   // .js may be omitted here
 
 console.log('Here is our import: '+x);
-console.log('Our ${x} again!');
+console.log(`Our ${x} again!`);
