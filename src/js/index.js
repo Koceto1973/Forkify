2cfb74a82f9016aa4bf1d0614311f30fce569d2d
TeamK <koceto1973@yahoo.ca>
2018-12-18 20:59:25 +0200

    controller/ state variable in, search conroller function in

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,28 @@
 
 import Search from './models/Search';
 
-var search = new Search('pizza');
-search.getResults();
-console.log(search);
+// global app state variable
+const state = {};
+
+const controlSearch = async () => {
+    // get the query from the view
+    const query='pizza'; 
+
+    if (query) {
+        // new search, add it to state
+        state.search = new Search(query);
+
+        // prepare UI for the result
+
+        // search for resipes
+        await state.search.getResults();
+    }
+
+    // results to UI
+    console.log(state.search.result);
+}
+
+document.querySelector('.search').addEventListener('submit',event => {
+    event.preventDefault(); // prevent the event from activation, so we wait serch results first
+    controlSearch();
+});
