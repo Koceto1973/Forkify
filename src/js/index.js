5052214842f917a53278c89fc1018ba860a00234
TeamK <koceto1973@yahoo.ca>
2018-12-19 02:02:13 +0200

    file names refactor

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 // app controller
 
-import Search from './models/Search';
-import * as searchView from './views/SearchView';
-import {elements} from './views/base';
+import Search from './models/m_search';
+import * as searchView from './views/v_search';
+import {elements} from './views/v_shortcuts';
 
 // global app state variable
-const state = {};
+const app_state = {};
 
 const controlSearch = async () => {
     // get the query from the view
@@ -13,19 +13,19 @@ const controlSearch = async () => {
 
     if (query) {
         // new search, add it to state
-        state.search = new Search(query);
+        app_state.search = new Search(query);
 
         // prepare UI for the result
         searchView.clearSearchField();
         searchView.clearResults();
 
         // search for resipes
-        await state.search.getResults();
+        await app_state.search.getResults();
     }
 
     // results to UI
     // console.log(state.search.result);
-    searchView.renderRecipes(state.search.result);    
+    searchView.renderRecipes(app_state.search.result);    
 }
 
 elements.searchForm.addEventListener('submit',event => {
--- src/js/views/v_search.js
+++ src/js/views/v_search.js
@@ -1,4 +1,4 @@
-import {elements} from './base';
+import {elements} from './v_shortcuts';
 
 export const getInput = () => {
     return elements.searchInput.value;
